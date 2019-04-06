2b7e633aa8695fed2775da7b46635a59bba1a4af
mrkiffie <mrkiffie@gmail.com>
2019-04-06 23:17:01 +0200

    Remove react from the lib bundle. Fix children reference

--- webpack.config.js
+++ webpack.config.js
@@ -27,11 +27,7 @@ module.exports =  {
       }
     ]
   },
-  resolve: {
-    alias: {
-        react: path.resolve('./node_modules/react'),
-        'react-dom': path.resolve('./node_modules/react-dom')
-      }
-    },
-
+  externals: {
+    react: 'react'
+  }
 };
